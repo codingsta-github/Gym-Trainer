42d260536eb874a3c1cedcf0623d4ed105b06cf9
coding-sta <personal.sta.bd@gmail.com>
2022-06-25 06:53:05 +0600

    service details

--- src/Hooks/useServiceCart.js
+++ src/Hooks/useServiceCart.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 const useServiceCart = ({id}) => {
   const [ServiceCart, setServiceCart] = useState({});
   useEffect(() => {
-    const url=`http://localhost:5000/ServiceCart/${id}`
+    const url=`https://calm-dusk-64094.herokuapp.com/ServiceCart/${id}`
     fetch(url)
     .then((res) =>res.json()
     .then(data=>setServiceCart(data))
--- src/Pages/ServiceCart/ServiceCart.js
+++ src/Pages/ServiceCart/ServiceCart.js
@@ -4,28 +4,50 @@ import ServiceCartBanner from "../../Assets/Banner/banner3.jpg";
 import { useParams } from "react-router-dom";
 import useServiceCart from "../../Hooks/useServiceCart";
 const ServiceCart = () => {
-    const id=useParams()
-    const [ServiceCart]=useServiceCart(id)
-    const {image,name,price}=ServiceCart
+  const id = useParams();
+  const [ServiceCart] = useServiceCart(id);
+  const {
+    _id,
+    image,
+    name,
+    price,
+    category,
+    instructor,
+    schedule,
+    short,
+    full,
+    slots,
+  } = ServiceCart;
+  const BuyNow = () => {};
   return (
     <div className="ServiceCart">
       <div className="ServiceCart-banner">
         <img src={ServiceCartBanner} alt="" />
-       <div className="head">
-       <h1>FITNESS CONSULTATION</h1>
-       </div>
+        <div className="head">
+          <h1>FITNESS CONSULTATION</h1>
+        </div>
       </div>
       <div className="ServiceCart-details">
         <div className="ServiceCart-details-img">
           <img src={image} alt="" />
         </div>
         <div className="ServiceCart-details-info">
-
+          <h1>${price}.00</h1>
+          <p>{short}</p>
+          <form onClick={BuyNow}>
+            <p id="label">Choose a schedule:</p>
+            <select id="slots" name="slots">
+              {slots?.map((slot) => (
+                <option value={slot}>{slot}</option>
+              ))}
+            </select>
+            <input className="buynow" type="submit" value="buy now"/>
+          </form>
+          <div className="straight-line"></div>
+          <h4>category: <span>{category}</span></h4>
         </div>
       </div>
-      <div className="ServiceCart-description">
-<p></p>
-      </div>
+      <div className="ServiceCart-description"></div>
     </div>
   );
 };
